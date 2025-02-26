9070cc1669445b11ac319547240de5cfbf79ccda
Xkrdve <119463013+Xkrdve@users.noreply.github.com>
2025-02-26 09:34:17 +0100

    listview

--- taquin/lib/pages/home.dart
+++ taquin/lib/pages/home.dart
@@ -5,7 +5,36 @@ class Home extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("TP2", style: TextStyle(color: Colors.white),),
+      ),
+
+      body: ListView(
+        children: [
+
+          Card(
+            color: Colors.blue,
+            child: ListTile(
+              // numéro de l'exercice
+              title: Text("Exo 2", style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
+              onTap: () {
+                Navigator.push(context,
+                  // remplacer par la page corresondante
+                  MaterialPageRoute(builder: (context) => Scaffold())
+                )
+              },
+            ),
+          )
+
+        ],
+      )
+
+    );
+
   }
 
 
