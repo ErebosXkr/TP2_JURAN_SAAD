8ea2156f6e098ed100920bb56ec35f4d86729846
lealine <lea.line.saad@gmail.com>
2025-03-03 17:17:54 +0100

    restructuration de l'interface de l'application

--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:taquin/pages/home.dart';
+import 'package:taquin/pages/accueil.dart';
+import 'package:taquin/pages/home.dart'; //page des exercices
+import 'package:taquin/pages/about.dart';
+import 'package:taquin/pages/exo7.dart'; //en attendant d'avoir la page taquin
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +11,66 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TP2',
-      home: const Home(),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int selectedIndex = 0;
+
+  final List<Widget> pages = [
+    const Accueil(),
+    const Exo7(),
+    const Home(),
+    const About(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: selectedIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.games),
+            label: 'Taquin',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Exercices',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'A propos',
+          ),
+        ],
+      ),
     );
   }
 }
