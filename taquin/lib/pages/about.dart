ddc6db7c695db526865b5892e4f057482ac93135
lealine <lea.line.saad@gmail.com>
2025-03-03 17:28:03 +0100

    à propos provisoire

--- taquin/lib/pages/about.dart
+++ taquin/lib/pages/about.dart
@@ -5,8 +5,22 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('A propos'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('A propos'),
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Cette application de taquin est un jeu de puzzle classique où le joueur doit réorganiser les tuiles pour former une image ou une séquence correcte. '
+            'Le jeu est conçu pour être à la fois amusant et éducatif, aidant les joueurs à améliorer leurs compétences en résolution de problèmes et leur logique. '
+            'Profitez de différents niveaux de difficulté et de diverses images à reconstituer. Bonne chance et amusez-vous bien !',
+            style: TextStyle(fontSize: 16.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
