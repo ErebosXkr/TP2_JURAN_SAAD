445c1ee34c0d889743fcbdb6ff517d5a24b3b209
lealine <lea.line.saad@gmail.com>
2025-03-05 09:45:44 +0100

    ajout de boutons de difficulté sur l'accueil

--- taquin/lib/pages/accueil.dart
+++ taquin/lib/pages/accueil.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:taquin/pages/exo7.dart';
 
-class Accueil extends StatelessWidget {
+class Accueil extends StatefulWidget {
   const Accueil({super.key});
+
+  @override
+  _AccueilState createState() => _AccueilState();
+}
+
+class _AccueilState extends State<Accueil> {
+  int _selectedDifficulty = 3; // Variable pour stocker le niveau de difficulté
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +22,62 @@ class Accueil extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Image de prévisualisation du jeu
+          
             Image.asset(
               'assets/preview_image.png', // Remplacez par le chemin de la bonne image
               width: 200,
               height: 200,
+            ),
+            const SizedBox(height: 20),
+            // Boutons de sélection de niveau de difficulté
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedDifficulty = 3;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _selectedDifficulty == 3 ? Colors.blue : Colors.grey,
+                  ),
+                  child: const Text('Débutant'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedDifficulty = 4;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _selectedDifficulty == 4 ? Colors.blue : Colors.grey,
+                  ),
+                  child: const Text('Intermédiaire'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedDifficulty = 5;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _selectedDifficulty == 5 ? Colors.blue : Colors.grey,
+                  ),
+                  child: const Text('Avancé'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedDifficulty = 6;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _selectedDifficulty == 6 ? Colors.blue : Colors.grey,
+                  ),
+                  child: const Text('Expérimenté'),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             // Bouton "Jouer"
@@ -27,6 +85,7 @@ class Accueil extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
+                  //MaterialPageRoute(builder: (context) => Exo7(difficulty: _selectedDifficulty)),
                   MaterialPageRoute(builder: (context) => const Exo7()),
                 );
               },
