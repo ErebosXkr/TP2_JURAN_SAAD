6ef2565c35eddf0060975f924e237ce788693cc8
lealine <lea.line.saad@gmail.com>
2025-03-06 21:47:51 +0100

    disposition des exos

--- taquin/lib/pages/home.dart
+++ taquin/lib/pages/home.dart
@@ -16,59 +16,138 @@ class Home extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text("TP2", style: TextStyle(color: Colors.white)),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: const Text("Exo 2", style: TextStyle(color: Colors.black)),
-            subtitle: const Text("Transformer une image"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ImageTransformer()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text("Exo 4", style: TextStyle(color: Colors.black)),
-            subtitle: const Text("Affichage d'une tuile (un morceau d'image)"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DisplayTileWidget()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text("Exo 5", style: TextStyle(color: Colors.black)),
-            subtitle: const Text("Génération du plateau de tuiles"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Exo5()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text("Exo 6", style: TextStyle(color: Colors.black)),
-            subtitle: const Text("Animation d'une tuile"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PositionedTiles()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text("Exo 7", style: TextStyle(color: Colors.black)),
-            subtitle: const Text("Jeu de taquin"),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Exo7()),
-              );
-            },
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: const Text("Exo 2", style: TextStyle(color: Colors.black)),
+                subtitle: const Text("Transformer une image"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ImageTransformer()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: const Text("Exo 4", style: TextStyle(color: Colors.black)),
+                subtitle: const Text("Affichage d'une tuile (un morceau d'image)"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DisplayTileWidget()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: const Text("Exo 5", style: TextStyle(color: Colors.black)),
+                subtitle: const Text("Génération du plateau de tuiles"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Exo5()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: const Text("Exo 6", style: TextStyle(color: Colors.black)),
+                subtitle: const Text("Animation d'une tuile"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PositionedTiles()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: const Text("Exo 7", style: TextStyle(color: Colors.black)),
+                subtitle: const Text("Jeu de taquin"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Exo7()),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
